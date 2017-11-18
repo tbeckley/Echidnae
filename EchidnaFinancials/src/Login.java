789a2c1e5e05016dec2306172d850127a3499b65
kaitlynp1206 <kaitlyn.p1206@gmail.com>
2017-11-18 17:06:02 -0500

    update login

--- EchidnaFinancials/src/Login.java
+++ EchidnaFinancials/src/Login.java
@@ -1,13 +1,17 @@
 import java.awt.EventQueue;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import java.awt.Color;
+
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 
 public class Login {
 	
@@ -16,6 +20,10 @@ public class Login {
 	private static final Color textColour = new Color(255, 255, 255);
 	private static JTextField userText;
 	private static JButton btnStartGame;
+	private static String username = "";
+	
+	private Image echidna;
+	private JLabel lblUsername;
 	
 	/**
 	 * Launch the application.
@@ -52,27 +60,42 @@ public class Login {
 		
 		//start game button
 		btnStartGame = new JButton("Start Game");
-		btnStartGame.setBounds(166, 336, 141, 35);
+		btnStartGame.setBounds(178, 348, 141, 35);
 		
 		
 		//image logo
 		ImageIcon echidna = new ImageIcon("echidnaLogo.png");
-		Image temp = echidna.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		Image temp = echidna.getImage().getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
 		echidna= new ImageIcon(temp);
 		JLabel imageEchidna = new JLabel(echidna);
-		imageEchidna.setBounds(137, 37, 200, 190);
+		imageEchidna.setBounds(147, 37, 200, 190);
 		
 		//username entry
 		userText = new JTextField(20);
-		userText.setBounds(144, 267, 186, 32);
+		userText.setBounds(154, 283, 186, 32);
 		
 
 		loginFrame.getContentPane().add(imageEchidna);
 		loginFrame.getContentPane().add(btnStartGame);
 		loginFrame.getContentPane().add(userText);
 		
+		lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(128, 247, 141, 26);
+		loginFrame.getContentPane().add(lblUsername);
+		
 		loginFrame.setBounds(100, 100, 500, 500);
 		loginFrame.setResizable(false);
 		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		btnStartGame.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+				username = userText.getText();
+				close();
+            }
+		});
+	}
+	
+	public void close() {
+		loginFrame.dispose();
 	}
 }
