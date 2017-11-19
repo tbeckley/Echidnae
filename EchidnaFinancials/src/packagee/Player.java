456b842c130298caf9441395b6312794d28ef46e
Thomas Beckley <tbeckley99@gmail.com>
2017-11-19 00:34:34 -0500

    Added get task functionality

--- EchidnaFinancials/src/packagee/Player.java
+++ EchidnaFinancials/src/packagee/Player.java
@@ -1,6 +1,9 @@
 package packagee;
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Player {
 	String name;
@@ -42,10 +45,47 @@ public class Player {
 		for(int i = 0; i < expenses.size(); i++) totalExpenses += expenses.get(i).cost;
 		return balance >= totalExpenses;
 	}
-	
+	public static ArrayList<TaskModel> getDayTasks(final int day)
+	{
+		ArrayList<TaskModel> tasks = new ArrayList<TaskModel>();
+		
+		String filePath = new File("").getAbsolutePath();
+		filePath = filePath + "\\resources\\day"+day;
+		System.out.println(filePath);
+		
+		File[] listOfFiles = new File(filePath).listFiles();
+		
+		for(int i = 0; i < listOfFiles.length; i++)
+		{
+			String s = "";
+			try
+			{
+				Scanner inputFile = new Scanner(listOfFiles[i]);
+				
+				while(inputFile.hasNext())
+					s = s.concat(inputFile.next());
+				
+				inputFile.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println("Could not find file");
+			}
+			
+			System.out.println(s);
+			
+			Gson gson = new Gson();
+			tasks.add(gson.fromJson(s, TaskModel.class));
+		}
+		return tasks;	
+	}
 	
 	public static void main(String[] args) throws Exception {
-		
+		ArrayList<TaskModel> tasks = getDayTasks(1);
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println(""+tasks.get(i).taskName);
+		}
 	}
 }
 
--- EchidnaFinancials/src/packagee/ScoreModel.java
+++ EchidnaFinancials/src/packagee/ScoreModel.java
@@ -1,7 +1,5 @@
 package packagee;
 
-import java.util.Date;
-
 public class ScoreModel {
 	public String UserName;
 	public long Score;
